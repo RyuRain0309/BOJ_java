02eb58f0f8e3e600445d902b40babf4aa2c8e116
RyuRain0309 <lyw3837@soongsil.ac.kr>
2023-02-08 01:17:16 +0900

    BOJ

--- Q9251.java
+++ Q9251.java
@@ -9,22 +9,20 @@ public class Q9251 {
         char[] B = br.readLine().toCharArray();
         int[][] dp = new int[A.length+1][B.length+1];
         for(int i = 0 ; i <= A.length ; i++){
-            dp[i][0] = i;
+            dp[i][0] = 0;
         }
         for(int i = 0 ; i <= B.length ; i++){
-            dp[0][i] = i;
+            dp[0][i] = 0;
         }
         for(int i = 1 ; i <= A.length ; i++){
             for(int j = 1 ; j <= B.length ; j++){
-                int val = (A[i-1] == B[j-1]) ? 0 : 1;
-                dp[i][j] = Math.min(dp[i-1][j-1] + val,Math.min(dp[i-1][j]+1,dp[i][j-1]+1));
+                if(A[i-1] == B[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
-        }
-        for(int i = 0 ; i <= A.length ; i++) {
-            for (int j = 0; j <= B.length; j++) {
-                System.out.print(dp[i][j]);
-            }
-            System.out.println();
         }
         System.out.print(dp[A.length][B.length]);
     }
