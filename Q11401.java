f671287d9fd0a3bfed0323d4c6857a5023c7bfa6
RyuRain0309 <lyw3837@soongsil.ac.kr>
2023-02-17 17:29:36 +0900

    BOJ

---
+++ Q11401.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q11401 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+    }
+}
