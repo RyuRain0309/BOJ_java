66b6d37e6002be3bc5509c461db26b3d16fa04de
RyuRain0309 <lyw3837@soongsil.ac.kr>
2023-02-23 20:37:44 +0900

    BOJ

--- Q11066.java
+++ Q11066.java
@@ -12,32 +12,25 @@ public class Q11066 {
         int T = Integer.parseInt(br.readLine());
         while (T-->0){
             int K = Integer.parseInt(br.readLine());
-            int[] arr = new int[K];
-            int[] sum = new int[K];
-            int[][] dp = new int[K][K];
+            int[] arr = new int[K+1];
+            int[] sum = new int[K+1];
+            int[][] dp = new int[K+1][K+1];
             st = new StringTokenizer(br.readLine()," ");
-            arr[0] = Integer.parseInt(st.nextToken());
-            sum[0] = arr[0];
-            for(int i = 1 ; i < K ; i++){
+            for(int i = 1 ; i <= K ; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
                 sum[i] = sum[i-1] + arr[i];
             }
 
             for(int n = 1 ; n <= K ; n++){
-                for(int from = 0 ; from+n < K ; from++){
+                for(int from = 1 ; from+n <= K ; from++){
                     int to = from+n;
                     dp[from][to] = Integer.MAX_VALUE;
                     for(int div = from ; div < to ; div++){
-                        if(from == 0){
-                            dp[from][to] = Math.min(dp[from][to],dp[from][div] + dp[div+1][to] + sum[to]);
-                        }
-                        else {
-                            dp[from][to] = Math.min(dp[from][to],dp[from][div] + dp[div+1][to] + sum[to] - sum[from - 1]);
-                        }
+                            dp[from][to] = Math.min(dp[from][to],dp[from][div] + dp[div+1][to] + sum[to] - sum[from-1]);
                     }
                 }
             }
-            sb.append(dp[1][K-1]).append("\n");
+            sb.append(dp[1][K]).append("\n");
         }
         System.out.print(sb);
     }
