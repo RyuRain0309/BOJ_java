6fcd6fe4a2e3e8e92abc6416e57b872d374a7bcc
RyuRain0309 <lyw3837@soongsil.ac.kr>
2023-03-09 19:13:18 +0900

    BOJ

--- Q1644.java
+++ Q1644.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Q1644 {
-    static final int MAX = 4_000_00;
-    static ArrayList<Long> primeSum = new ArrayList<>();
+    static ArrayList<Integer> prime = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        getPrime();
+        getPrime(N+1);
         int res = 0;
         int sPoint = 0;
-        int ePoint = 1;
-        while (ePoint < primeSum.size() && primeSum.get(ePoint) - primeSum.get(ePoint-1) <= N){
-            long sum = primeSum.get(ePoint) - primeSum.get(sPoint);
-            //System.out.println(primeSum.get(ePoint) - primeSum.get(ePoint-1) + " " + primeSum.get(ePoint)+ " " + primeSum.get(sPoint) + " " + sum);
+        int ePoint = 0;
+        while (ePoint < prime.size() && prime.get(ePoint) <= N){
+            int sum = 0;
+            for(int i = sPoint ; i <= ePoint ; i++){
+                sum += prime.get(i);
+                if(sum > N){
+                    break;
+                }
+            }
             if(sum > N){
                 sPoint++;
             }
@@ -31,22 +35,21 @@ public class Q1644 {
         System.out.print(res);
     }
 
-    private static void getPrime() {
+    private static void getPrime(int MAX) {
         boolean[] isPrime = new boolean[MAX+1];
         Arrays.fill(isPrime,true);
+        isPrime[1] = false;
         for(int i = 3 ; i < Math.sqrt(MAX+1) ; i+= 2){
-            isPrime[1] = false;
             if(isPrime[i]){
                 for(int j = i + i ; j <= MAX ; j += i  ){
                     isPrime[j] = false;
                 }
             }
         }
-        primeSum.add(0L);
-        primeSum.add(2L);
+        prime.add(2);
         for(int i = 3 ; i <= MAX ; i+= 2){
             if(isPrime[i]){
-                primeSum.add(primeSum.get(primeSum.size() - 1) + i);
+                prime.add(i);
             }
         }
     }
