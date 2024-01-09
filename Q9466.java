9c114ab118803823b05772316b774f0628319744
ryurain <lyw3837@soongsil.ac.kr>
2024-01-09 10:28:34 +0900

    BOJ

--- Q9466.java
+++ Q9466.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Q9466 {
@@ -9,9 +8,10 @@ public class Q9466 {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     static int N;
+    static int cnt;
     static int[] arr;
     static boolean[] isVisited;
-    static boolean[] canNotMakeTeam;
+    static boolean[] done;
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
@@ -23,60 +23,37 @@ public class Q9466 {
 
     private static void solve() throws IOException {
         N = Integer.parseInt(br.readLine());
+        cnt = 0;
         arr = new int[N + 1];
         isVisited = new boolean[N + 1];
-        canNotMakeTeam = new boolean[N + 1];
+        done = new boolean[N + 1];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i <= N; i++) {
-            if (arr[i] == i) {
-                isVisited[i] = true;
-            }
-        }
         for (int i = 1; i <= N; i++) {
-            if (isVisited[i] || canNotMakeTeam[i]) {
-                continue;
-            }
             dfs(i);
         }
-        int cnt = 0;
-        for (int i = 1; i <= N; i++) {
-            if (!isVisited[i]) {
-                cnt++;
-            }
-        }
-        sb.append(cnt).append("\n");
+        sb.append(N - cnt).append("\n");
     }
 
-    private static void dfs(int start) {
-        ArrayList<Integer> t = new ArrayList<>();
-        t.add(start);
-        while (true) {
-            if (t.contains(arr[t.getLast()])){
-                int index =t.indexOf(arr[t.getLast()]);
-                for (int i = 0; i < index; i++) {
-                    canNotMakeTeam[t.get(i)] = true;
-                }
-                for (int i = index; i < t.size() ; i++) {
-                    isVisited[t.get(i)] = true;
-                }
-                break;
-            }
-            if (isVisited[arr[t.getLast()]]){
-                for (Integer integer : t) {
-                    canNotMakeTeam[integer] = true;
-                }
-                break;
-            }
-            if (canNotMakeTeam[arr[t.getLast()]]){
-                for (Integer integer : t) {
-                    canNotMakeTeam[integer] = true;
-                }
-                break;
-            }
-            t.add(arr[t.getLast()]);
+    private static void dfs(int now) {
+        if(isVisited[now]){
+            return;
         }
+
+        isVisited[now] = true;
+        int next = arr[now];
+
+        if(!isVisited[next])
+            dfs(next);
+        else {
+            if(!done[next]) {
+                cnt++;
+                for(int i=next; i != now; i = arr[i])
+                    cnt++;
+            }
+        }
+        done[now] = true;
     }
 }
